7d2456e95dcb726cc355a4721d5a595540995d3e
tech-rajesh <ece.rajesh@gmail.com>
2021-06-12 11:05:18 +0530

    added lib & added new conf
    added lib & added new conf

---
+++ conf/conf_base_jenkins_demo_allureReport.js
@@ -0,0 +1,29 @@
+
+var AllureReporter = require('jasmine-allure-reporter');
+jasmine.getEnv().addReporter(new AllureReporter({
+  resultsDir: 'allure-results'
+}));
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+    //'browserName': 'firefox'
+
+    
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../tests/calculator_pom.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
