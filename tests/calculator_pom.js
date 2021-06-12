481b3689e676527b4bf211c9416623dae550e4ff
tech-rajesh <ece.rajesh@gmail.com>
2021-06-12 12:40:14 +0530

    added test and updated test
    added test and updated test

--- tests/calculator_pom.js
+++ tests/calculator_pom.js
@@ -2,7 +2,7 @@ let homepage = require('../pages/homepage');
 describe('demo calulator test', function () {
 
 
-    it('addition test', function () {
+    it('addition test 1 digit number', function () {
         // browser.get('https://juliemr.github.io/protractor-demo/');
         homepage.getAppURL('https://juliemr.github.io/protractor-demo/')
         expect(browser.getCurrentUrl()).toBe('https://juliemr.github.io/protractor-demo/');
@@ -18,12 +18,12 @@ describe('demo calulator test', function () {
     });
 
 
-it('addition test', function () {
+it('addition test 2 digit number', function () {
         // browser.get('https://juliemr.github.io/protractor-demo/');
         homepage.getAppURL('https://juliemr.github.io/protractor-demo/')
         expect(browser.getCurrentUrl()).toBe('https://juliemr.github.io/protractor-demo/');
 
-        homepage.enterFirstNumber('8')
+        homepage.enterFirstNumber('18')
         homepage.enterSecondNumber('19')
         browser.sleep(2000)
         homepage.clickGo();
@@ -33,7 +33,22 @@ it('addition test', function () {
 
     });
 
-it('addition test', function () {
+it('addition test 3 digit number', function () {
+        // browser.get('https://juliemr.github.io/protractor-demo/');
+        homepage.getAppURL('https://juliemr.github.io/protractor-demo/')
+        expect(browser.getCurrentUrl()).toBe('https://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('819')
+        homepage.enterSecondNumber('919')
+        browser.sleep(2000)
+        homepage.clickGo();
+
+        
+        browser.sleep(5000)
+
+    });
+
+it('addition test 4 digit number', function () {
         // browser.get('https://juliemr.github.io/protractor-demo/');
         homepage.getAppURL('https://juliemr.github.io/protractor-demo/')
         expect(browser.getCurrentUrl()).toBe('https://juliemr.github.io/protractor-demo/');
