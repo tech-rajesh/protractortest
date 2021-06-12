8b46655f950d4be89f520925edec4f81d659a775
tech-rajesh <ece.rajesh@gmail.com>
2021-06-12 10:19:19 +0530

    added new testcase
    added new testcase

--- tests/calculator_pom.js
+++ tests/calculator_pom.js
@@ -33,5 +33,20 @@ it('addition test', function () {
 
     });
 
+it('addition test', function () {
+        // browser.get('https://juliemr.github.io/protractor-demo/');
+        homepage.getAppURL('https://juliemr.github.io/protractor-demo/')
+        expect(browser.getCurrentUrl()).toBe('https://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('181')
+        homepage.enterSecondNumber('191')
+        browser.sleep(2000)
+        homepage.clickGo();
+
+        
+        browser.sleep(5000)
+
+    });
+
 
 });
